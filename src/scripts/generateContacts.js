29659b0315369fbc0df5063e524c0a330ad939c2
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-06-05 18:10:29 +0200

    generatecontacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  let contacts = [];
+  for (let i = 0; i <= number; i++) {
+    contacts.push(createFakeContact());
+  }
+  //   console.log(JSON.stringify(contacts));
+  try {
+    await fs.appendFile(PATH_DB, JSON.stringify(contacts));
+    console.log('Дані успішно записані у файл.');
+  } catch (err) {
+    console.error('Помилка запису у файл:', err);
+  }
+};
 
 await generateContacts(5);
